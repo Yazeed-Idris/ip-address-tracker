bd939b13f54d7057af4bc1fe1dc1b0a2ca44ca49
yazed <s201730270@kfupm.edu.sa>
2023-04-14 11:45:28 +0300

    - Completed functionality of the map only styling and animations are left

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,92 @@
 <script setup>
-
 import Search from "./components/Search.vue";
+import {onMounted, ref, watch} from "vue";
 import InformationDisplay from "./components/InformationDisplay.vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+const apiKey = 'at_qQiXnY3DU4Hi5kB1ZJrAirzYiwAAc'
+const searchQuery = ref({})
+const ipAddress = ref(null)
+const location = ref(null)
+const timezone = ref(null)
+const latitude = ref(null)
+const longitude = ref(null)
+const isp = ref(null)
+let mapDiv;
+
+async function getClientIP() {
+    const response = await fetch('https://api.ipify.org?format=json')
+        .then(response => response.json());
+    searchQuery.value.type = 'ip'
+    searchQuery.value.address = response.ip
+}
+
+
+async function getIPData() {
+
+    let getURL = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=' + apiKey + '&domain=' + searchQuery.value.address
+    if (searchQuery.value.type === 'ip') {
+        getURL = 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=' + apiKey + '&ipAddress=' + searchQuery.value.address
+    }
+    console.log('getURL is:', getURL)
+    const response = await fetch(getURL)
+        .then(response => response.json());
+    console.log('response is:', response)
+    location.value = response.location.city + ', ' + response.location.country;
+    timezone.value = 'UTC ' + response.location.timezone;
+    isp.value = response.isp;
+    ipAddress.value = response.ip;
+    latitude.value = response.location.lat;
+    longitude.value = response.location.lng;
+}
+
+async function callAPIs() {
+    await getClientIP()
+    await getIPData()
+}
+
+callAPIs()
+
+onMounted(() => {
+    mapDiv = L.map('mapContainer').setView([24.7136, 46.6753], 13)
+    const accessToken = 'pk.eyJ1IjoieWF6ZWVkLWlkcmlzIiwiYSI6ImNsZ2c3dHR2aDA5MWQzcm8wZnRodWJmYTgifQ.Svuo1STYnrpWpTZKombMdg'
+    L.tileLayer(
+        'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+            tileSize: 512,
+            zoomOffset: -1,
+            attribution: '© <a href="https://www.mapbox.com/contribute/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(mapDiv);
+
+})
+
+watch(location, () => {
+    if (location.value) {
+        mapDiv.setView([latitude.value, longitude.value], 13)
+        L.marker([latitude.value, longitude.value]).addTo(mapDiv);
+    }
+})
+
+function onIPChanged($event) {
+    console.log('ip is:', $event.type, $event.address)
+    searchQuery.value = $event
+    getIPData()
+}
+
 </script>
 
 <template>
     <div class="flex flex-col h-full w-full">
-        <div class="bg-Mobile-Background sm:bg-Desktop-Background relative w-full bg-no-repeat bg-cover">
+        <div class="bg-Mobile-Background sm:bg-Desktop-Background relative w-full bg-no-repeat bg-cover z-20">
             <img src="/pattern-bg-mobile.png" alt="" class="invisible">
             <div class="absolute w-full h-full top-0 flex flex-col items-center px-6">
                 <h1 class="text-white font-medium text-2xl my-6">IP Address Tracker</h1>
-                <Search/>
+                <Search @ip-changed="onIPChanged"/>
                 <div class="absolute bottom-0 translate-y-1/2 w-full px-6 lg:px-44">
-                <InformationDisplay ip-address="192.212.174.101" location="Brooklyn, NY 10001" timezone="UTC-5:00" isp="SpaceX Starlink" />
+                <InformationDisplay v-if="location" :ip-address="ipAddress" :location="location" :timezone="timezone" :isp="isp" />
                 </div>
             </div>
         </div>
-        <div class="grow bg-red-900 h-full">
-
-        </div>
+        <div class="grow bg-red-900 h-full z-10"  id="mapContainer"></div>
     </div>
 
 </template>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,29 @@
 <script setup>
-import { ref } from "vue";
-
-const searchIP = ref("");
+import { ref, defineEmits } from "vue";
+const emits = defineEmits(["ipChanged"]);
+const searchQuery = ref("");
+const ipValidationRegex = new RegExp(
+  "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+);
+const domainValidationRegex = new RegExp(
+  "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$"
+);
 
 function search() {
-  console.log("search ip:", searchIP.value);
+    if (ipValidationRegex.test(searchQuery.value)) {
+       emits("ipChanged", { address: searchQuery.value, type: "ip" });
+       return;
+    }
+    if (domainValidationRegex.test(searchQuery.value)) {
+       emits("ipChanged", { address: searchQuery.value, type: "domain" });
+    }
 }
 
 </script>
 
 <template>
   <div class="flex justify-between items-center h-14 w-full max-w-xl">
-    <input type="text" class="h-full grow rounded-l-2xl px-4 lg:px-6 placeholder:font-[500] focus:outline-none" placeholder="Search for any IP or domain" v-model="searchIP" />
+    <input type="text" class="h-full grow rounded-l-2xl px-4 lg:px-6 placeholder:font-[500] focus:outline-none" placeholder="Search for any IP or domain" v-model="searchQuery" />
     <button @click="search" class="bg-black grid place-items-center rounded-r-2xl w-16 h-full">
         <img src="/icon-arrow.svg"  alt="arrow right icon">
     </button>
